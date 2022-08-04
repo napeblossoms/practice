719f4d8bca2a0a9e7ebd313cd7de58bc28c8225a
napeblossoms <napeblossoms@gmail.com>
2022-08-04 14:25:42 +0000

    [sample2]

--- repeat7.rb
+++ repeat7.rb
@@ -3,3 +3,16 @@ while num <= 10 do
     puts "Number is: " + num.to_s
     num = num + 1
 end
+
+#if,while組み合わせ
+num = 0
+while num < 5 do
+    if num == 0
+        puts num.to_s + " is Zero"
+    elsif num > 5
+        puts num.to_s + " is over 5"
+    else
+        puts num.to_s + " is under 5"
+    end
+    num = num + 1
+end
